240583353bca1f51c290239b27d93180d6215c4a
= <aymancoom3@gmail.com>
2023-09-18 14:21:04 +0300

    New Card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
                     @include('custcoom-lb')
                     @include('custcoom-tm')
                     {{--  --}}
-                    
+                    @include('attendance')
                 </div>
             </div>
         </div>
