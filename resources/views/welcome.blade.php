9164f3652cd513d5f32fc29475056391502455fc
= <aymancoom3@gmail.com>
2023-10-29 10:07:34 +0200

    Nothing New

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
                 <img src="{{ asset('img/coom.jpg') }}" alt="">
             </div>
             <div class="mt-16">
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                     @include('excel-django')
                     @include('dash-analytica')
                     @include('custcoom-lb')
