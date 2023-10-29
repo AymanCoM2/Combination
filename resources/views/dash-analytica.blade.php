08bb816eb07b24a8a20a0ec48e3578c531b582c8
= <aymancoom3@gmail.com>
2023-10-29 10:00:25 +0200

    Refactor the Code

--- resources/views/dash-analytica.blade.php
+++ resources/views/dash-analytica.blade.php
@@ -8,14 +8,11 @@
                     d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z" />
             </svg>
         </div>
-
         <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Dash Analytics</h2>
-
         <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
             For Query and Their Results
         </p>
     </div>
-
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
         class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,38 +4,32 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>2Coom Portal</title>
-
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <!-- Styles -->
     @include('style-tag')
 </head>
 
 <body class="antialiased">
     <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        class="min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         <div class="max-w-5xl mx-auto p-6 lg:p-8">
             <div class="flex justify-center">
                 <img src="{{ asset('img/coom.jpg') }}" alt="">
             </div>
-
             <div class="mt-16">
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                     @include('excel-django')
                     @include('dash-analytica')
                     @include('custcoom-lb')
                     @include('custcoom-tm')
-                    {{--  --}}
                     @include('attendance')
                     @include('commerce')
                 </div>
             </div>
         </div>
     </div>
+
 </body>
 
 </html>
