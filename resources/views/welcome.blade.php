8faabf34175f5466e7640715e4bd6d75a583b239
Ayman <ayman.salah@2coom.com>
2023-12-24 14:46:36 +0200

    Removed Redundant Links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,12 +20,12 @@
             <div class="mt-16">
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                     @include('excel-django')
-                    @include('dash-analytica')
+                    {{-- @include('dash-analytica') --}}
                     @include('custcoom-lb')
                     @include('custcoom-tm')
-                    @include('attendance')
-                    @include('commerce')
-                    @include('pivotAnalytica')
+                    {{-- @include('attendance') --}}
+                    {{-- @include('commerce') --}}
+                    {{-- @include('pivotAnalytica') --}}
                 </div>
             </div>
         </div>
